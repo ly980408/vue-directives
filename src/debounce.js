adbd7e2baaa7707ad87a28fb02e1696a09ab84dc
liyang <liyang@wikiflyer.cn>
2022-02-15 14:18:12 +0800

    refactor(debounce):  support 'delay' parameter

--- src/debounce.js
+++ src/debounce.js
@@ -1,25 +1,30 @@
+const defaultEventType = 'click'
+const defaultDelay = 300
+
 /**
  * v-debounce
  * @description 事件防抖
  * @example
- *  - 默认为 click 事件 `<button v-debounce="onClick" ></button>`
- *  - 也可以指定事件类型进行绑定 `<input v-debounce:input="onInput" />`
- * @todo 增加 delay 参数...
+ *  默认用法  `<button v-debounce="onClick"></button>`
+ *  指定事件类型，默认为 click  `<input v-debounce:input="onInput" />`
+ *  指定事件延迟，默认为 300    `<input v-debounce:input="onInput" debounce-delay="500" />`
  */
-
 const debounce = {
   inserted(el, binding) {
-    const eventType = binding.arg || 'click'
+    const fn = binding.value
+    const eventType = binding.arg || defaultEventType
+    const delay = Number(el.getAttribute('debounce-delay')) || defaultDelay
+
     let timer
     el.addEventListener(eventType, () => {
       if (timer) {
         clearTimeout(timer)
       }
       timer = setTimeout(() => {
-        binding.value()
-      }, 300)
+        fn()
+      }, delay)
     })
-  }
+  },
 }
 
 export default debounce
